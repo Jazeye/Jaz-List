3948e5f77f977cc337497e275803284ab0334c58
Jazeye <jaseelp394@gmail.com>
2024-08-19 21:23:33 +0530

    ascci changed

--- src/pass-gen.sh
+++ src/pass-gen.sh
@@ -40,12 +40,12 @@ landing_page() {
   local message="Welcome to the Password List Generator and Steganography Cracker"
   local developer="Developed by Jaseel | Version 0.1"
   local art="
- ____            _          ____               
-|  _ \ ___ _ __ | |_ __ _  |  _ \ __ _ ___ ___ 
-| |_) / _ \ '_ \| __/ _` | | |_) / _` / __/ __|
-|  __/  __/ | | | || (_| | |  __/ (_| \__ \__ \
-|_|   \___|_| |_|\__\__,_| |_|   \__,_|___/___/
-                                                         
+     _                _     _     _   
+    | | __ _ ____    | |   (_)___| |_ 
+ _  | |/ _` |_  /____| |   | / __| __|
+| |_| | (_| |/ /_____| |___| \__ \ |_ 
+ \___/ \__,_/___|    |_____|_|___/\__|
+                                                                                             
   "
   clear
   echo "$art"
